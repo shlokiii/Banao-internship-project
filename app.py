377858d49dd11ebe50dc7510cd19432794c405d9
shlokiii <139153519+shlokiii@users.noreply.github.com>
2024-12-18 23:05:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # HTML template for your homepage
+    return render_template("index.html")  
 
 if __name__ == "__main__":
-    # Use Render's $PORT environment variable or default to 5000 for local testing
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
